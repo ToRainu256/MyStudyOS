e1ebbbed995a3cd67741ff798908b2f2bdf5dc3c
ToRainu256 <torainu256@gmail.com>
2019-12-23 17:22:13 +0900

    my os goes to 2nd stage

--- src/boot/boot.s
+++ src/boot/boot.s
@@ -45,22 +45,37 @@ ipl:
   ;--------------------------------------------------
   ; output value
   ;--------------------------------------------------
-    cdecl itoa, 8086, .s1, 8, 10, 0b0001  ; "     8086"
-    cdecl puts, .s1 
+  ;  cdecl itoa, 8086, .s1, 8, 10, 0b0001  ; "     8086"
+  ;  cdecl puts, .s1 
 
-    cdecl itoa, 8086, .s1, 8, 10, 0b0011  ; "+    8086"
-    cdecl puts, .s1
+  ;  cdecl itoa, 8086, .s1, 8, 10, 0b0011  ; "+    8086"
+  ;  cdecl puts, .s1
 
-    cdecl itoa, -8086, .s1, 8, 10, 0b0001 ; "-    8086"
-    cdecl puts, .s1
+  ;  cdecl itoa, -8086, .s1, 8, 10, 0b0001 ; "-    8086"
+  ;  cdecl puts, .s1
 
-    cdecl itoa, -1,    .s1, 8, 10, 0b0001 ; "-       1"
-    cdecl puts, .s1
+  ;  cdecl itoa, -1,    .s1, 8, 10, 0b0001 ; "-       1"
+  ;  cdecl puts, .s1
 
   ;--------------------------------------------------
-  ; reboot
+  ; read next 512 byte
   ;--------------------------------------------------
-    cdecl reboot                         ; never return
+      mov ah, 0x02                            ; AH = read op
+      mov al, 1                               ; al = number of read sector
+      mov cx, 0x0002                          ; CX = cylinder/sector
+      mov dh, 0x00                            ; DH = head position
+      mov dl, [BOOT.DRIVE]                    ; DL = drive numver
+      mov bx, 0x7C00 + 512                    ; BX = offset
+      int 0x13                                ; if (CF = BIOS(0x13, 0x02))
+.10Q: jnc .10E                                ; {
+.10T: cdecl puts, .e0                         ;   puts(.e0);
+      call reboot                             ;   reboot();
+.10E:                                         ; }
+
+  ;--------------------------------------------------
+  ; boot processing goes NEXT STAGE!!!!
+  ;--------------------------------------------------
+      jmp stage_2                                 ; 
 
   ;--------------------------------------------------
   ; End of process
@@ -71,8 +86,8 @@ ipl:
   ;--------------------------------------------------
   ; Data
   ;--------------------------------------------------
-.s0 db "Booting...", 0x0A, 0x0D, 0
-.s1 db "         ", 0x0A, 0x0D, 0
+.s0 db "Booting...", 0x0A, 0x0D, 0 
+.e0 db "Error:sector read", 0
 
 ALIGN 2, db 0
 BOOT:                   ; infomation about boot drive
@@ -89,8 +104,31 @@ BOOT:                   ; infomation about boot drive
 ;**********************************************
 ; Boot Flag(End of Head 512 bytes)
 ;**********************************************
-  times 510 - ($ - $$) db 0x00
-  db 0x55, 0xAA
+    times 510 - ($ - $$) db 0x00
+    db 0x55, 0xAA
+
+;**********************************************
+; boot process 2nd stage
+;**********************************************
+stage_2:
+
+  ;--------------------------------------------------
+  ; output literal
+  ;--------------------------------------------------
+    cdecl puts, .s0
 
 
+  ;--------------------------------------------------
+  ; end of prossecing
+  ;--------------------------------------------------
+    jmp $
 
+  ;--------------------------------------------------
+  ; data
+  ;--------------------------------------------------
+.s0 db "2nd stage...", 0x0A, 0x0D, 0
+
+;**********************************************
+; Padding
+;**********************************************
+    times (1024 * 8) - ($ - $$) db 0 ;8Kbyte
